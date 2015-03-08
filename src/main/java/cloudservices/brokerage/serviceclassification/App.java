ef1ae3ea1899e9688ade68de6218a973d42c432c
Arash Khodadadi <arashk1368@gmail.com>
2015-03-08 18:12:24 -0400

    Project Created

---
+++ src/main/java/cloudservices/brokerage/serviceclassification/App.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2014 Arash khodadadi.
+ * <http://www.arashkhodadadi.com/>
+ */
+package cloudservices.brokerage.serviceclassification;
+
+/**
+ *
+ * @author Arash Khodadadi <http://www.arashkhodadadi.com/>
+ */
+public class App {
+    
+}
