6688f317a0528b0861a40746ad071ec08e35f848
Arash Khodadadi <arashk1368@gmail.com>
2015-06-12 00:14:49 -0400

    Reading report files and creating result files added

--- src/main/java/cloudservices/brokerage/serviceclassification/ReportsAnalayzer.java
+++ src/main/java/cloudservices/brokerage/serviceclassification/ReportsAnalayzer.java
@@ -6,12 +6,15 @@ package cloudservices.brokerage.serviceclassification;
 
 import cloudservices.brokerage.commons.utils.file_utils.DirectoryUtil;
 import cloudservices.brokerage.commons.utils.logging.LoggerSetup;
+import java.io.File;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.util.Pair;
 
 /**
  *
@@ -20,10 +23,60 @@ import java.util.logging.Logger;
 public class ReportsAnalayzer {
 
     private final static Logger LOGGER = Logger.getLogger(ReportsAnalayzer.class.getName());
+    private final List<File> reportFiles;
+    private final String reportsFolderAddress;
+    private final String extension;
+
+    public ReportsAnalayzer(String reportsFolderAddress, String extension) {
+        this.reportsFolderAddress = reportsFolderAddress;
+        this.extension = extension;
+        this.reportFiles = DirectoryUtil.getAllFiles(reportsFolderAddress, extension);
+    }
 
     public static void main(String[] argv) {
-        createLogFile();
-        ReportsAnalayzer analyzer = new ReportsAnalayzer();
+        try {
+            createLogFile();
+
+            LOGGER.log(Level.SEVERE, "Analyzing Report Files Start...");
+
+            ReportsAnalayzer analyzer = new ReportsAnalayzer("reports", "txt");
+            analyzer.analyzeAll2FoldFiles();
+
+        } catch (Exception ex) {
+            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+
+    public void analyzeAll2FoldFiles() throws Exception {
+        Pair<File, File> pair = getNextFilesPair();
+        while (pair != null) {
+            File file1 = pair.getKey();
+            File file2 = pair.getValue();
+            LOGGER.log(Level.INFO, "Analyzing {0} and {1}", new Object[]{file1.getPath(), file2.getPath()});
+            File average = this.createResultFile(file1);
+            LOGGER.log(Level.INFO, "Created Result File in {0}", average.getPath());
+            pair = getNextFilesPair();
+        }
+    }
+
+    private Pair<File, File> getNextFilesPair() {
+        for (File reportFile : reportFiles) {
+
+            String path1 = reportFile.getPath();
+            LOGGER.log(Level.FINE, "Analyzing {0}", path1);
+            if (path1.contains("2Fold")) {
+                File file2 = new File(path1.replace("-2Fold", ""));
+                if (!reportFiles.contains(file2)) {
+                    LOGGER.log(Level.INFO, "There is no pair for: {0}", path1);
+                } else {
+                    LOGGER.log(Level.FINE, "Found Pair: {0}", file2);
+                    reportFiles.remove(reportFile);
+                    reportFiles.remove(file2);
+                    return new Pair(reportFile, file2);
+                }
+            }
+        }
+        return null;
     }
 
     private static boolean createLogFile() {
@@ -40,5 +93,24 @@ public class ReportsAnalayzer {
             LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
             return false;
         }
+    }
+
+    private File createResultFile(File file1) throws Exception {
+        String address = file1.getPath().replace(file1.getName(), "");
+        address += "results";
+        DirectoryUtil.createDir(address);
+        address += "/";
+        String fileName = file1.getName().replace("." + this.extension, "");
+        if (fileName.contains("-2Fold")) {
+            fileName = fileName.replace("-2Fold", "");
+        }
+        fileName += "-Average";
+        File result = new File(address + fileName + "." + this.extension);
+        if (result.exists()) {
+            LOGGER.log(Level.INFO, "Result file already exists : {0}", result.getPath());
+            result.delete();
+        }
+        result.createNewFile();
+        return result;
     }
 }
