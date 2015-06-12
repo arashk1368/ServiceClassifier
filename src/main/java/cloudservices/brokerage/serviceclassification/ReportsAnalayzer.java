cfbd6158576c95296e8643f9e822214c17b0911a
Arash Khodadadi <arashk1368@gmail.com>
2015-06-11 22:30:18 -0400

    Reports analyzer class added

---
+++ src/main/java/cloudservices/brokerage/serviceclassification/ReportsAnalayzer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Arash khodadadi.
+ * <http://www.arashkhodadadi.com/>
+ */
+package cloudservices.brokerage.serviceclassification;
+
+import cloudservices.brokerage.commons.utils.file_utils.DirectoryUtil;
+import cloudservices.brokerage.commons.utils.logging.LoggerSetup;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Arash Khodadadi <http://www.arashkhodadadi.com/>
+ */
+public class ReportsAnalayzer {
+
+    private final static Logger LOGGER = Logger.getLogger(ReportsAnalayzer.class.getName());
+
+    public static void main(String[] argv) {
+        createLogFile();
+        ReportsAnalayzer analyzer = new ReportsAnalayzer();
+    }
+
+    private static boolean createLogFile() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm");
+            Calendar cal = Calendar.getInstance();
+            sb.append(dateFormat.format(cal.getTime()));
+            String filename = sb.toString();
+            DirectoryUtil.createDir("logs");
+            LoggerSetup.setup("logs/" + filename + ".txt", "logs/" + filename + ".html", Level.INFO);
+            return true;
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+            return false;
+        }
+    }
+}
