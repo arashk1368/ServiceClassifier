4706dcb4d7503e029cd0c87bceab6b5a3bf07140
Arash Khodadadi <arashk1368@gmail.com>
2015-06-14 14:52:19 -0400

    Report Row added

---
+++ src/main/java/cloudservices/brokerage/serviceclassification/ReportRow.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2014 Arash khodadadi.
+ * <http://www.arashkhodadadi.com/>
+ */
+package cloudservices.brokerage.serviceclassification;
+
+/**
+ *
+ * @author Arash Khodadadi <http://www.arashkhodadadi.com/>
+ */
+public class ReportRow {
+
+    private String guess;
+    private int run;
+    private String config;
+    private int good;
+    private int bad;
+    private double precision;
+    private double recall;
+    private double mca;
+    private double ms;
+    private boolean isClassResult;
+    private String className;
+
+    public ReportRow() {
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+
+    public int getRun() {
+        return run;
+    }
+
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+    public String getConfig() {
+        return config;
+    }
+
+    public void setConfig(String config) {
+        this.config = config;
+    }
+
+    public int getGood() {
+        return good;
+    }
+
+    public void setGood(int good) {
+        this.good = good;
+    }
+
+    public int getBad() {
+        return bad;
+    }
+
+    public void setBad(int bad) {
+        this.bad = bad;
+    }
+
+    public double getPrecision() {
+        return precision;
+    }
+
+    public void setPrecision(double precision) {
+        this.precision = precision;
+    }
+
+    public double getRecall() {
+        return recall;
+    }
+
+    public void setRecall(double recall) {
+        this.recall = recall;
+    }
+
+    public double getMca() {
+        return mca;
+    }
+
+    public void setMca(double mca) {
+        this.mca = mca;
+    }
+
+    public double getMs() {
+        return ms;
+    }
+
+    public void setMs(double ms) {
+        this.ms = ms;
+    }
+
+    public boolean isIsClassResult() {
+        return isClassResult;
+    }
+
+    public void setIsClassResult(boolean isClassResult) {
+        this.isClassResult = isClassResult;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getKey() {
+        return this.guess.concat(this.config);
+    }
+
+    @Override
+    public String toString() {
+        return "ReportRow{" + "guess=" + guess + ", run=" + run + ", config=" + config + ", good=" + good + ", bad=" + bad + ", precision=" + precision + ", recall=" + recall + ", mca=" + mca + ", ms=" + ms + ", isClassResult=" + isClassResult + ", className=" + className + '}';
+    }
+}
