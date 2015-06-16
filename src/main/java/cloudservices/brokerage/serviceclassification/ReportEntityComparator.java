d97e15e3a5f4e434164781e74e903de85223658f
Arash Khodadadi <arashk1368@gmail.com>
2015-06-15 20:17:08 -0400

    Added sorting based on mca and timing if precision is equal

--- src/main/java/cloudservices/brokerage/serviceclassification/ReportEntityComparator.java
+++ src/main/java/cloudservices/brokerage/serviceclassification/ReportEntityComparator.java
@@ -25,11 +25,23 @@ public class ReportEntityComparator implements Comparator<String> {
         ReportRow rr1 = base.get(key1).getConfigResult();
         ReportRow rr2 = base.get(key2).getConfigResult();
         if (rr1.getGuess().equals(rr2.getGuess())) {
-            if (rr1.compareTo(rr2) >= 0) {
+            if (rr1.compareTo(rr2) > 0) {
                 return -1;
-            } else {
-                return 1;
-            } // returning 0 would merge keys
+            } else if (rr1.compareTo(rr2) < 0) {
+                return 1;// returning 0 would merge keys
+            } else { // Equal precision
+                if (rr1.getMca() > rr2.getMca()) {
+                    return -1;
+                } else if (rr1.getMca() < rr2.getMca()) {
+                    return 1;
+                } else { // MCA and precision equal
+                    if (rr1.getMs() < rr2.getMs()) {
+                        return -1;
+                    } else {
+                        return 1;
+                    }
+                }
+            }
         } else if (rr1.getGuess().equals("1st")) {
             return -1;
         } else {
